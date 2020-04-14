2cafbecd5e61cea3873ae7ff096136a865b83750
Jarno van Wezel <jarnovwezel@me.com>
2020-04-14 12:24:26 +0200

    contact script finished

--- public/contact.php
+++ public/contact.php
@@ -2,6 +2,10 @@
 
 $data = json_decode(file_get_contents('php://input'), true);
 
+if (empty($data['name']) || empty($data['email']) || empty($data['subject']) || empty($data['email'])) {
+    throw new Exception('Something went wrong, please try again.');
+}
+
 $parts = [
     'from: ' . $data['name'] . '<' . $data['email'] . '>',
     '',
@@ -9,6 +13,6 @@ $parts = [
     $data['message']
 ];
 
-mail('mail@jarn.io', $data['subject'], implode("\n", $parts));
+mail('contact@jarn.io', $data['subject'], implode("\n", $parts));
 
 echo 'send!';
